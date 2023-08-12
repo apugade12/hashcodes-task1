88e86200948da16fd7e2a5827bbc235645f38fb1
apugade12 <133955303+apugade12@users.noreply.github.com>
2023-08-12 22:07:37 +0530

    Add files via upload

---
+++ level1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+typedef std::map<std::string, int> StringIntMap;
+void count_words(std::istream &in, StringIntMap &words)
+{
+    std::string text;
+    while (in >> text)
+    {
+        ++words[text];
+    }
+}
+
+int main(int argc, char **argv)
+{
+    std::ifstream in("file.txt");
+    StringIntMap words_map;
+    count_words(in, words_map);
+
+    for (StringIntMap::iterator it = words_map.begin(); it != words_map.end(); ++it)
+    {
+        std::cout << it->first << " occured " << it->second << std::endl;
+    }
+}
+
